bd4bfbe07fd27dd82d286e6d222381916cb94dd0
xavier quishpe <quishpexavier27@gmail.com>
2024-11-08 13:57:57 -0500

    feat: :tada: create two documents
    create two documents , type js and hmtl

---
+++ app.js
@@ -0,0 +1,6 @@
+let div= document.getElementById("buttonhello")
+let button= document.createElement("button")
+button.innerText="click here"
+
+button.addEventListener('click',()=> alert ('holitas uwu'))
+div.appendChild(button)
